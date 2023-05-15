80f8680696740f0c8206f7988fd7f9d298afda59
Freddy <freddy.wei0810@gmail.com>
2023-05-16 00:54:22 +0800

    feat: get get_srt() work and add docstrings for functions

--- app.py
+++ app.py
@@ -8,36 +8,52 @@ import os
 
 # get api key from environment variables
 load_dotenv()
-openai.api_key = os.getenv('OPENAI_WHISPER_API_KEY')
+openai.api_key = os.getenv("OPENAI_WHISPER_API_KEY")
 
 
-def get_srt():
-    file_path = 'C:\\Users\\loveh\\Desktop\\壓縮後\\_1_遊戲介紹.mp4'
-    with open(file_path, "rb") as audio_file:
+def get_srt(src_path, dst_path, response_format="srt", language="en"):
+    '''
+        Get srt file from video.
+    '''
+    with open(src_path, "rb") as audio_file:
         transcription = openai.Audio.translate(
             model="whisper-1",
             file=audio_file,
-            response_format="srt",
-            language="en",
+            response_format=response_format,
+            language=language,
         )
-        with open(f"en_out.srt", "w", encoding="utf-8") as f:
+        with open(dst_path, "w", encoding="utf-8") as f:
             f.write(transcription)
+    return transcription
 
 
 def get_audio():
+    '''
+        Get audio from srt file.
+    '''
     pass
 
 
 def get_muted_video():
+    '''
+        Get muted video from original video.
+    '''
     pass
 
 
 def combine_muted_video_and_audio():
+    '''
+        Combine muted video and audio.
+    '''
     pass
 
 
 def main():
-    pass
+    transcription = get_srt(
+        src_path='C:\\Users\\loveh\\Desktop\\test_project\\src_videos\\_1_遊戲介紹.mp4', 
+        dst_path='C:\\Users\\loveh\\Desktop\\test_project\\srts\\_1_遊戲介紹.srt',
+        response_format='srt', language='en'
+    )
 
 
 if __name__ == '__main__':
